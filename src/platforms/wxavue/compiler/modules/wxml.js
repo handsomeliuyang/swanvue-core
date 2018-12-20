307c3f6772d410cb76fbe1a8e4d08a0fed176f92
liuyang <liuyang@58.com>
2018-12-20 15:10:05 +0800

    支持属性的Mustache的转换为v-bind

--- src/platforms/wxavue/compiler/modules/wxml.js
+++ src/platforms/wxavue/compiler/modules/wxml.js
@@ -10,18 +10,69 @@ import {
   addRawAttr,
   getAndRemoveAttr
 } from 'compiler/helpers'
+import {parseFilters} from "../../../../compiler/parser/filter-parser";
+
+// 用于判断是否包含{{ }}的正则表达式
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
+function transformMustache (el: ASTElement, options: CompilerOptions) {
+  const replaceAttrs = []
+  for(var i=0; i<el.attrsList.length; i++){
+    var attr = el.attrsList[i];
+
+    const tagRE = defaultTagRE
+    if (!tagRE.test(attr.value)) {
+      continue
+    }
+
+    const tokens = []
+    // const rawTokens = []
+    let lastIndex = tagRE.lastIndex = 0
+    let match, index
+    while ((match = tagRE.exec(attr.value))) {
+      index = match.index
+      // push text token
+      if (index > lastIndex) {
+        tokens.push(`'${attr.value.slice(lastIndex, index)}'`)
+      }
+      // tag token
+      const exp = match[1].trim()
+      tokens.push(`(${exp})`)
+      lastIndex = index + match[0].length
+    }
+    if (lastIndex < attr.value.length) {
+      tokens.push(`'${attr.value.slice(lastIndex)}'`)
+    }
+
+    replaceAttrs.push({name: attr.name, value: tokens.join('+')});
+  }
+  for(let i=0; i<replaceAttrs.length; i++){
+    const attr = replaceAttrs[i];
+
+    getAndRemoveAttr(el, attr.name, true)
+    addRawAttr(el, 'v-bind:'+attr.name, attr.value);
+  }
+}
+
+function transformBindtap(el: ASTElement, options: CompilerOptions) {
+  let val = getAndRemoveAttr(el, 'bindtap', true)
+  if (val) {
+    addRawAttr(el, 'v-on:click', val+'($event)');
+  }
+
+}
 
 function preTransformNode (el: ASTElement, options: CompilerOptions) {
+  // view ==> div, text ==> div
   if(el.tag === 'view' || el.tag === 'text'){
     el.tag = 'div';
   }
 
-  if(el.tag === 'button') {
-    let val = getAndRemoveAttr(el, 'bindtap', true)
-    if (val) {
-      addRawAttr(el, 'v-on:click', val);
-    }
-  }
+  // bindtap ==> v-on:click
+  transformBindtap(el, options)
+
+  // xx="item-{{id}}" ==> v-bind:id="'item-'+id"
+  transformMustache(el, options)
+
 }
 
 export default {
